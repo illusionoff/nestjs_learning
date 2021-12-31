332d800bc426d9b1a75096e7183f32e4e3d37a39
MiniPC <venom12@inbox.ru>
2021-12-31 12:28:30 +0300

    Get Get :id

---
+++ src/products/products.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('products')
+export class ProductsController {
+
+  @Get()
+  getAll() {
+    return 'getAll'
+  }
+
+  @Get(':id')
+  getOne(@Param() params) {
+    return 'getOne' + params.id
+  }
+}
