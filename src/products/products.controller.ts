d933e7cb84ead11993072a89d71962a94ff376ce
MiniPC <venom12@inbox.ru>
2021-12-31 12:59:33 +0300

    controller Post Delete(remove) Put (update) in Postman

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product-dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -8,8 +10,29 @@ export class ProductsController {
     return 'getAll'
   }
 
+  // @Get(':id')
+  // getOne(@Param() params) {
+  //   return 'getOne' + params.id
+  // }
+
   @Get(':id')
-  getOne(@Param() params) {
-    return 'getOne' + params.id
+  getOne(@Param('id') id: string): string {
+    return 'getOne' + id
+  }
+
+  @Post()
+  create(@Body() createProductDto: CreateProductDto): string {
+    return `Title: ${createProductDto.title} Price: ${createProductDto.price}`
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return 'Remove ' + id
+
+  }
+
+  @Put(':id')
+  update(@Body() updateProductDto: UpdateProductDto, @Param('id') id: string) {
+    return 'Update ' + id
   }
 }
