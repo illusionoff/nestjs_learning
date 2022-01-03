f28c2847a58c2cc42c535a35026a71ce8d2c77f1
MiniPC <venom12@inbox.ru>
2022-01-03 16:17:01 +0300

    good conect MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,8 @@ import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [ProductsModule,
-    MongooseModule.forRoot('mongodb+srv://alex:pasale16@cluster0.fgr0d.mongodb.net/products?retryWrites=true&w=majority')
+    // mongodb+srv://alex:<password>@cluster0.fgr0d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+    MongooseModule.forRoot('mongodb+srv://alex:pasalex16@cluster0.fgr0d.mongodb.net/products?retryWrites=true&w=majority')
   ],
   controllers: [AppController],
   providers: [AppService],
